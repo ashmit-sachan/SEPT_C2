0ae8f35d68ef4b03a8468815eda41265092f4841
ashmit-sachan <ashmit666@gmail.com>
2022-10-12 08:41:18 +1100

    Completed

--- account/src/test/java/com/s3873827/account/AccountApplicationTests.java
+++ account/src/test/java/com/s3873827/account/AccountApplicationTests.java
@@ -1,13 +1,33 @@
 package com.s3873827.account;
 
+import com.s3873827.account.enums.AccountType;
+import com.s3873827.account.model.Account;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Timestamp;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SpringBootTest
 class AccountApplicationTests {
 
     @Test
-    void contextLoads() {
+    @DisplayName("Test 1: Should pass if Account has sufficient balance, ie. more than 100$")
+    void hasValidAge() {
+        Account account = new Account(0L, AccountType.SAVING, "7896123786489681", "Max", "999", new Timestamp(System.currentTimeMillis()));
+
+        Assertions.assertTrue(Integer.parseInt(account.getBalance()) > 99);
+    }
+
+    @Test
+    @DisplayName("Test 2: Should pass if Account number has 16 digits")
+    void hasComplete10DigitPhoneNumber() {
+        Account account = new Account(0L, AccountType.SAVING, "1234567890112233", "Max", "999", new Timestamp(System.currentTimeMillis()));
+
+        Assertions.assertEquals(16, account.getAccNumber().length());
     }
 
 }
--- person/src/test/java/com/s3873827/person/PersonApplicationTests.java
+++ person/src/test/java/com/s3873827/person/PersonApplicationTests.java
@@ -1,13 +1,42 @@
 package com.s3873827.person;
 
+import com.s3873827.person.model.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @SpringBootTest
 class PersonApplicationTests {
 
     @Test
-    void contextLoads() {
+    @DisplayName("Test 1: Should pass if person's age is in the a desired time period (Last 100 years and not in Future)")
+    void hasValidAge() {
+        Person person = new Person(0L, "Ashmit", "Address", "3052", "32", "Job", "example@gmail.com", "1234567890");
+
+        Assertions.assertTrue(Integer.parseInt(person.getAge()) > 0 || Integer.parseInt(person.getAge()) < 101);
+    }
+
+    @Test
+    @DisplayName("Test 2: Should pass if person's phone number has 10 digits")
+    void hasComplete10DigitPhoneNumber() {
+        Person person = new Person(0L, "Ashmit", "Address", "3052", "32", "Job", "example@gmail.com", "1234567890");
+
+        Assertions.assertEquals(10, person.getPhoneno().length());
+    }
+
+    @Test
+    @DisplayName("Test 3: Should pass if person's email has a valid syntax")
+    void hasValidEmailAddress() {
+        final Pattern VALID_EMAIL_ADDRESS_REGEX =
+        Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+        Person person = new Person(0L, "Ashmit", "Address", "3052", "32", "Job", "example#$csd{}gmail.com", "1234567890");
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(person.getEmail());
+
+        Assertions.assertFalse(matcher.find());
     }
 
 }
